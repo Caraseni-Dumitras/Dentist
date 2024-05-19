b426e207b6e15cc48822dadca5a7414f1bc107ad
Caraseni-Dumitras <caraseni.dumitras@gmail.com>
2024-05-19 12:49:55 +0300

    Set up database and project architecture

---
+++ Dentist/Infrastructure/Configurations.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class Configurations
+{
+    public static void ConfigureInfrastructure(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ??
+                               throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        
+        serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString));
+        serviceCollection.AddDatabaseDeveloperPageExceptionFilter();
+
+        serviceCollection.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+    }
+}
---
+++ Dentist/Web/Program.cs
@@ -0,0 +1,41 @@
+using Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Configuration.AddJsonFile("AppData/appsettings.json", optional: true, reloadOnChange: true);
+builder.Configuration.AddJsonFile("AppData/appsettings.Local.json", optional: true, reloadOnChange: true);
+builder.Configuration.AddJsonFile("AppData/appsettings.Development.json", optional: true, reloadOnChange: true);
+
+// configure layers
+builder.Services.ConfigureInfrastructure(builder.Configuration);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
